61997a179ac847ab7a11d37dc75b8894c69d455c
Quang Than <quang.than@velox.no>
2022-08-23 08:33:01 +0700

    Add more files

---
+++ api/src/migrations/20220816100805-add-workplace-to-plans.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.addColumn('plans', 'workplace', {
+      type: Sequelize.ENUM('Norway', 'Nha Trang', 'HCM', 'Other'),
+      defaultValue: 'Other'
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.removeColumn('plans', 'workplace');
+  }
+};
